30b2f9fc1fbf4225b405eadf159675132adf1caf
Bsargent23 <164093282+Bsargent23@users.noreply.github.com>
2024-04-23 22:06:47 +0000

    new change

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// setup firebase app and firestore database
+const firebaseConfig = {
+  apiKey: "AIzaSyDqlsu1JVtO8uvvGo77B-A1QIzfew0t0sU",
+  authDomain: "madlibs-7f4f8.firebaseapp.com",
+  projectId: "madlibs-7f4f8",
+  storageBucket: "madlibs-7f4f8.appspot.com",
+  messagingSenderId: "354466264589",
+  appId: "1:354466264589:web:9a2c3785e73c968de2c744"
+};
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+console.log("firebase setup complete!");
+
+
+
+
+
+
 function madlib() {
 
   console.log("madlib() called");
